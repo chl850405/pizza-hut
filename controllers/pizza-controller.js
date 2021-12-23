f29ed1e983967c75ca93c33cb7c8d42e55d4d8a5
chl850405 <christimarchetti@gmail.com>
2021-12-23 13:18:56 -0500

    validation

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -4,12 +4,12 @@ const pizzaController = {
   // get all pizzas
   getAllPizza(req, res) {
     Pizza.find({})
-    .populate({
-      path: 'comments',
-      select: '-__v'
-    })
-    .select('-__v')
-    .sort({ _id: -1 })
+      .populate({
+        path: 'comments',
+        select: '-__v'
+      })
+      .select('-__v')
+      .sort({ _id: -1 })
       .then(dbPizzaData => res.json(dbPizzaData))
       .catch(err => {
         console.log(err);
@@ -41,7 +41,7 @@ const pizzaController = {
 
   // update pizza by id
   updatePizza({ params, body }, res) {
-    Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true })
+    Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
       .then(dbPizzaData => {
         if (!dbPizzaData) {
           res.status(404).json({ message: 'No pizza found with this id!' });
@@ -61,4 +61,3 @@ const pizzaController = {
 };
 
 module.exports = pizzaController;
-
